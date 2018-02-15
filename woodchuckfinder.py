b02133e033965f8d8baa1977246e003446e75ea9
necrotoxin44 <theunnamed97@gmail.com>
2018-02-15 18:34:21 -0500

    Added loops to search and find names in files.

--- woodchuckfinder.py
+++ woodchuckfinder.py
@@ -6,20 +6,20 @@ parser.add_argument("animal_file", help="file of animal names")
 parser.add_argument("noun_file", help="file of nouns")
 parser.add_argument("verb_file", help="file of verbs")
 args=parser.parse_args()
-
+#optional argument for output file?
 try: #receive input files from command line
-    with open(args.animal_file) as animal_file, open(args.noun_file) as noun_file, open(args.verb_file) as verb_file:
-        #read each file into a list?
-        for line in animal_file:
-            print(line)
-        for line in noun_file:
-            print(line)
-        for line in verb_file:
-            print(line)
+    with open(args.animal_file) as animal_file, open(args.noun_file) as noun_file, open(args.verb_file) as verb_file, open("woodchuckoutput.txt", "w+") as output:
+        for noun in noun_file:
+            n = (noun.strip()).lower()
+            for verb in verb_file:
+                v = (verb.strip()).lower()
+                for animal in animal_file:
+                    a = animal.strip()
+                    if(((n + v) in a.lower()) or ((n + ' ' + v) in a.lower())):
+                        print(a)
+                        output.write(a + '\n')
+                animal_file.seek(0)
+            verb_file.seek(0)
+
 except OSError:
     sys.exit("file not found")
-
-#for each line of animal_names, which is assumed to be a single name
-    #search for noun in line
-        #if noun found, sound for a following verbs
-            #if verb also found, write line to woodchuck_animals.txt
