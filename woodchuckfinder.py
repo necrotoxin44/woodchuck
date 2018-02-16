0d474804865650e8e9d00294031977b9888f2c0c
necrotoxin44 <theunnamed97@gmail.com>
2018-02-16 17:03:05 -0500

    back to working

--- woodchuckfinder.py
+++ woodchuckfinder.py
@@ -8,18 +8,8 @@ args=parser.parse_args()
 #optional argument for output file?
 #print progress percentage?
 
-def file_len(file):
-    file.seek(0)
-    line_count = 0
-    for line in file:
-        line_count += 1
-    file.seek(0)
-    return line_count
-
 try:
     with open(args.animal_file) as animal_file, open(args.noun_file) as noun_file, open(args.verb_file) as verb_file, open("woodchuckoutput.txt", "w+") as output:
-        total_animals = file_len(animal_file)
-        count = 0
         for animal in animal_file:
             a = (animal.strip()).lower()
             for noun in noun_file:
@@ -29,12 +19,9 @@ try:
                         v = (verb.strip()).lower()
                         if(((n + v) in a.lower()) or ((n + ' ' + v) in a.lower())):
                             print(a)
-                            output.write(a + '\n')
+                            output.write(animal.strip() + '\n')
                     verb_file.seek(0)
             noun_file.seek(0)
-            count += 1
-
-            print("Percentage: " + str(round(100 * (count/total_animals))) + '\n')
 
 except OSError:
     sys.exit("file not found")
