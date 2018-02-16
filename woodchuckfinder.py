234df8e181a37b0c79586d85c5258df912c12d98
necrotoxin44 <theunnamed97@gmail.com>
2018-02-16 16:30:02 -0500

    test of original loop structure

--- woodchuckfinder.py
+++ woodchuckfinder.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("animal_file", help="file of animal names")
@@ -7,8 +8,20 @@ parser.add_argument("verb_file", help="file of verbs")
 args=parser.parse_args()
 #optional argument for output file?
 #print progress percentage?
+
+def file_len(file):
+    file.seek(0)
+    line_count = 0
+    for line in file:
+        line_count += 1
+    file.seek(0)
+    return line_count
+
 try:
     with open(args.animal_file) as animal_file, open(args.noun_file) as noun_file, open(args.verb_file) as verb_file, open("woodchuckoutput.txt", "w+") as output:
+        perc_per_loop = 100 * (1 / file_len(noun_file))
+        curr_count = 0
+        start = time.time()
         for noun in noun_file:
             n = (noun.strip()).lower()
             for verb in verb_file:
@@ -20,6 +33,10 @@ try:
                         output.write(a + '\n')
                 animal_file.seek(0)
             verb_file.seek(0)
+            curr_count += 1
+            end = time.time()
+            print(str(perc_per_loop / (end - start)) + " %/sec")
+            start = time.time()
 
 except OSError:
     sys.exit("file not found")
